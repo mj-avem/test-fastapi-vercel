74accbedc7cef77b5c738bcb7da73e7d1083b595
marcelojsilva <marcelojosedasilva@gmail.com>
2023-08-03 17:48:50 -0300

    aider: Create routes to recenseador.
    # Aider chat conversation:
    
    USER: crie o dto, model e repositorie para a tabela CREATE TABLE `recenseadores` (
    	`id` int NOT NULL AUTO_INCREMENT,
    	`nome` varchar(100) NOT NULL,
    	`usuario` varchar(50) NOT NULL,
    	`senha` varchar(255) NOT NULL,
    	`email` varchar(100) NOT NULL,
    	`data_nascimento` date NOT NULL,
    	`ativo` tinyint(1) NOT NULL,
    	PRIMARY KEY (`id`),
    	UNIQUE KEY `usuario` (`usuario`),
    	UNIQUE KEY `email` (`email`),
    	KEY `ix_recenseadores_id` (`id`)
    )
    
    ASSISTANT: It seems like you already have the DTO, Model, and Repository for the `recenseadores` table. They are `RecenseadorDto`, `RecenseadorModel`, and `RecenseadorRepository` respectively.
    
    If you want to create a new DTO, Model, and Repository for a different table, please provide the table name and its fields.
    
    USER: create routes to recenseador
    
    ASSISTANT: To create routes for the `recenseador`, we need to modify the `src/route.py` file. We will add new routes for creating, retrieving, updating, and deleting a `recenseador`.
    
    First, we need to import the `RecenseadorController` and create an instance of it. Then, we will add the routes.
    
    Here are the changes:
    
    ```python
    src/route.py
    <<<<<<< ORIGINAL
    from fastapi import APIRouter
    from src.controllers.PersonController import PersonController
    from src.dtos.PersonDto import PersonDto
    
    router = APIRouter()
    person_controller = PersonController()
    =======
    from fastapi import APIRouter
    from src.controllers.PersonController import PersonController
    from src.controllers.RecenseadorController import RecenseadorController
    from src.dtos.PersonDto import PersonDto
    from src.dtos.RecenseadorDto import RecenseadorDto
    
    router = APIRouter()
    person_controller = PersonController()
    recenseador_controller = RecenseadorController()
    >>>>>>> UPDATED
    ```
    
    ```python
    src/route.py
    <<<<<<< ORIGINAL
    @router.delete("/person/{person_id}")
    async def delete_person(person_id: int):
        return person_controller.delete_person(person_id)
    =======
    @router.delete("/person/{person_id}")
    async def delete_person(person_id: int):
        return person_controller.delete_person(person_id)
    
    @router.post("/recenseador")
    async def create_recenseador(recenseador_dto: RecenseadorDto):
        return recenseador_controller.create_recenseador(recenseador_dto)
    
    @router.get("/recenseador/{recenseador_id}")
    async def get_recenseador(recenseador_id: int):
        return recenseador_controller.get_recenseador(recenseador_id)
    
    @router.put("/recenseador/{recenseador_id}")
    async def update_recenseador(recenseador_id: int, recenseador_dto: RecenseadorDto):
        return recenseador_controller.update_recenseador(recenseador_id, recenseador_dto)
    
    @router.delete("/recenseador/{recenseador_id}")
    async def delete_recenseador(recenseador_id: int):
        return recenseador_controller.delete_recenseador(recenseador_id)
    >>>>>>> UPDATED
    ```
    
    Please note that you need to create the `RecenseadorController` class and its methods (`create_recenseador`, `get_recenseador`, `update_recenseador`, `delete_recenseador`) in the `src/controllers/RecenseadorController.py` file. If you need help with that, please let me know.

--- src/route.py
+++ src/route.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from src.controllers.PersonController import PersonController
+from src.controllers.RecenseadorController import RecenseadorController
 from src.dtos.PersonDto import PersonDto
+from src.dtos.RecenseadorDto import RecenseadorDto
 
 router = APIRouter()
 person_controller = PersonController()
+recenseador_controller = RecenseadorController()
 
 @router.get("/")
 async def root():
@@ -24,3 +27,19 @@ async def update_person(person_id: int, person_dto: PersonDto):
 @router.delete("/person/{person_id}")
 async def delete_person(person_id: int):
     return person_controller.delete_person(person_id)
+
+@router.post("/recenseador")
+async def create_recenseador(recenseador_dto: RecenseadorDto):
+    return recenseador_controller.create_recenseador(recenseador_dto)
+
+@router.get("/recenseador/{recenseador_id}")
+async def get_recenseador(recenseador_id: int):
+    return recenseador_controller.get_recenseador(recenseador_id)
+
+@router.put("/recenseador/{recenseador_id}")
+async def update_recenseador(recenseador_id: int, recenseador_dto: RecenseadorDto):
+    return recenseador_controller.update_recenseador(recenseador_id, recenseador_dto)
+
+@router.delete("/recenseador/{recenseador_id}")
+async def delete_recenseador(recenseador_id: int):
+    return recenseador_controller.delete_recenseador(recenseador_id)
