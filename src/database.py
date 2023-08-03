d64e3799346d87efe9bd230e7f3da61332311b96
marcelojsilva <marcelojosedasilva@gmail.com>
2023-08-03 19:58:07 -0300

    Removed the port parameter from the Database class constructor.

--- src/database.py
+++ src/database.py
@@ -10,7 +10,7 @@ class Database:
             1, 
             10,
             host=os.getenv('DATABASE_HOST'),
-            port=os.getenv('DATABASE_PORT'),
+            # port=os.getenv('DATABASE_PORT'),
             dbname=os.getenv('DATABASE_NAME'),
             user=os.getenv('DATABASE_USER'),
             password=os.getenv('DATABASE_PASSWORD')
