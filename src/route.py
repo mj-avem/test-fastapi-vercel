c3465e05cbcd13e0538cb310690fd02e1548e01d
marcelojsilva <marcelojosedasilva@gmail.com>
2023-08-03 18:03:06 -0300

    Deleted PersonController, PersonRepository, PersonService, and related routes and dependencies.
    # Aider chat conversation:

--- src/route.py
+++ src/route.py
@@ -1,32 +1,13 @@
 from fastapi import APIRouter
-from src.controllers.PersonController import PersonController
 from src.controllers.RecenseadorController import RecenseadorController
-from src.dtos.PersonDto import PersonDto
 from src.dtos.RecenseadorDto import RecenseadorDto
 
 router = APIRouter()
-person_controller = PersonController()
 recenseador_controller = RecenseadorController()
 
 @router.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@router.post("/person")
-async def create_person(person_dto: PersonDto):
-    return person_controller.create_person(person_dto)
-
-@router.get("/person/{person_id}")
-async def get_person(person_id: int):
-    return person_controller.get_person(person_id)
-
-@router.put("/person/{person_id}")
-async def update_person(person_id: int, person_dto: PersonDto):
-    return person_controller.update_person(person_id, person_dto)
-
-@router.delete("/person/{person_id}")
-async def delete_person(person_id: int):
-    return person_controller.delete_person(person_id)
+    return {"message": "Ok"}
 
 @router.post("/recenseador")
 async def create_recenseador(recenseador_dto: RecenseadorDto):
