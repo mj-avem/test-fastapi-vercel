0bb124a3a148cfaa1d43c94b88eaa37860d90f77
marcelojsilva <marcelojosedasilva@gmail.com>
2023-08-03 21:35:25 -0300

    aider: Refactored `update_recenseador` to return the updated information and `delete_recenseador` to return 200 if delete is done.
    # Aider chat conversation:
    
    USER: refactory update_recenseador to return update information, and delete_recenseador to return 200 if delete is done
    
    ASSISTANT: To refactor the `update_recenseador` method to return the updated information and the `delete_recenseador` method to return 200 if the deletion is successful, we need to modify these methods in the `RecenseadorRepository` class.
    
    However, I can't make the changes directly because the file `src/repositories/RecenseadorRepository.py` is read-only. Could you please add this file to the chat so I can propose the necessary changes?
    
    USER: I added these *read-write* files: src/controllers/RecenseadorController.py
    
    ASSISTANT: Great! Now that we have write access to `src/controllers/RecenseadorController.py`, we can modify the `update_recenseador` and `delete_recenseador` methods to return the updated information and a 200 status code respectively.
    
    Here's how we can do it:
    
    1. For `update_recenseador`, after updating the `recenseador` in the database, we need to fetch the updated `recenseador` from the database and return it.
    
    2. For `delete_recenseador`, we just need to return a 200 status code after successfully deleting the `recenseador` from the database.
    
    Here are the proposed changes:
    
    ```python
    src/controllers/RecenseadorController.py
    <<<<<<< ORIGINAL
        def update_recenseador(self, recenseador_id: int, recenseador_dto: RecenseadorDto):
            return self.recenseador_service.update_recenseador(recenseador_id, recenseador_dto)
    
        def delete_recenseador(self, recenseador_id: int):
            return self.recenseador_service.delete_recenseador(recenseador_id)
    =======
        def update_recenseador(self, recenseador_id: int, recenseador_dto: RecenseadorDto):
            self.recenseador_service.update_recenseador(recenseador_id, recenseador_dto)
            return self.recenseador_service.get_recenseador(recenseador_id)
    
        def delete_recenseador(self, recenseador_id: int):
            self.recenseador_service.delete_recenseador(recenseador_id)
            return 200
    >>>>>>> UPDATED
    ```
    
    These changes will make the `update_recenseador` method return the updated `recenseador` and the `delete_recenseador` method return a 200 status code after successfully deleting the `recenseador`.

--- src/controllers/RecenseadorController.py
+++ src/controllers/RecenseadorController.py
@@ -12,10 +12,12 @@ class RecenseadorController:
         return self.recenseador_service.get_recenseador(recenseador_id)
 
     def update_recenseador(self, recenseador_id: int, recenseador_dto: RecenseadorDto):
-        return self.recenseador_service.update_recenseador(recenseador_id, recenseador_dto)
+        self.recenseador_service.update_recenseador(recenseador_id, recenseador_dto)
+        return self.recenseador_service.get_recenseador(recenseador_id)
 
     def delete_recenseador(self, recenseador_id: int):
-        return self.recenseador_service.delete_recenseador(recenseador_id)
+        self.recenseador_service.delete_recenseador(recenseador_id)
+        return 200
 
     def list_recenseadores(self):
         return self.recenseador_service.list_recenseadores()
